1f0744a317604d6871e22e34a2649716824cfc62
Jeffrey-Meijer <jeffreymeijer11@gmail.com>
2022-04-03 00:10:43 +0200

    Basic structure implemented

--- bungalowpark/views.py
+++ bungalowpark/views.py
@@ -1,7 +1,17 @@
 #import x
 
+from flask import render_template
 from bungalowpark import app
 
 @app.route('/')
-def index():
-    return 'test'
+def home_page():
+    return render_template("home.html")
+
+@app.route("/bungalows/<bungalow_id>")
+def single_bungalow_page(bungalow_id):
+  return render_template("bungalow/single.html", bungalow_id=bungalow_id)
+
+@app.route("/bungalows")
+def archive_bungalow_page():
+  return render_template("bungalow/archive.html")
+
